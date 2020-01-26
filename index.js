67c976aae9fc7fa7ad1cce26d21ef901e53df731
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-01-25 20:01:08 -0800

    Set up react

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 require("dotenv").config();
 const express = require("express");
 const app = express();
+const path = require("path");
 const bodyParser = require("body-parser");
 const User = require("./controllers/user");
-const port = process.env.PORT || 3001;
+const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
+app.use(express.static("public"));
 //basic auth routes
 app.post("/api/register", User.registerUser);
 

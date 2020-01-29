763e1bb5abaeeaddaddfb2a91de79e75a12d5c61
Brian Hambleton <bhambl3t0n1@gmail.com>
2020-01-29 07:50:54 -0800

    User can create notes

--- client/components/book-notes.jsx
+++ client/components/book-notes.jsx
@@ -5,6 +5,9 @@ import "../styles/book-notes.css";
 
 export default function BookNotes({ match }) {
   const [book, setBook] = useState({});
+  const [notes, setNotes] = useState([]);
+  const [toggle, setToggle] = useState(false);
+  const [content, setContent] = useState("");
 
   const getBook = () => {
     axios
@@ -15,13 +18,70 @@ export default function BookNotes({ match }) {
       .catch(err => console.error(err));
   };
 
+  const getNotes = () => {
+    axios
+      .get(`/api/notes/book/${match.params.bookId}`)
+      .then(res => {
+        setNotes(res.data);
+      })
+      .catch(err => console.error(err));
+  };
+
+  const toggleTrueFalse = () => {
+    setToggle(!toggle);
+  };
+
+  const createNote = () => {
+    axios
+      .post(`/api/notes/book/${match.params.bookId}`, {
+        content
+      })
+      .then(res => {
+        console.log(res.data);
+        getNotes();
+      })
+      .catch(err => console.error(err));
+  };
+
   useEffect(() => {
     getBook();
+    getNotes();
   }, []);
 
   return (
-    <div id="book-notes-container">
-      <p id="book-title">{book.title}</p>
-    </div>
+    <>
+      <div id="book-notes-container">
+        <p id="book-title">{book.title}</p>
+        <p>by</p>
+        <p>{book.author}</p>
+        <button onClick={toggleTrueFalse} id="add-note">
+          Add Note
+        </button>
+        <div>
+          {toggle ? (
+            <>
+              <textarea
+                placeholder="write your note"
+                onChange={e => setContent(e.target.value)}
+                name="content"
+                id="content"
+                cols="30"
+                rows="10"
+              />
+              <button onClick={createNote} id="add-note">
+                Post
+              </button>
+            </>
+          ) : (
+            ""
+          )}
+        </div>
+      </div>
+      <ol id="book-notes-list">
+        {notes.map(note => {
+          return <li>{note.content}</li>;
+        })}
+      </ol>
+    </>
   );
 }
